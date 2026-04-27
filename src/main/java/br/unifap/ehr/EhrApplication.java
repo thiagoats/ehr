61bea392709dfa8e06c2b0c273a8f166fdb0d933
Thiago Pinheiro do Nascimento <thiagopn.dev@gmail.com>
2026-04-27 14:44:47 -0300

    Project start

---
+++ src/main/java/br/unifap/ehr/EhrApplication.java
@@ -0,0 +1,22 @@
+package br.unifap.ehr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Application main class
+ * @author Thiago Pinheiro do Nascimento
+ * @since 0.1
+**/
+@SpringBootApplication
+public class EhrApplication {
+
+	/**
+	 * Application main method
+	 * @author Thiago Pinheiro do Nascimento
+	 * @since 0.1
+	**/
+	public static void main(String[] args) {
+		SpringApplication.run(EhrApplication.class, args);
+	}
+}
---
+++ src/test/java/br/unifap/ehr/EhrApplicationTests.java
@@ -0,0 +1,23 @@
+package br.unifap.ehr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Main test class
+ * @author Thiago Pinheiro do Nascimento
+ * @since 0.1
+**/
+@SpringBootTest
+class EhrApplicationTests {
+
+	/**
+	 * Main test method
+	 * @author Thiago Pinheiro do Nascimento
+	 * @since 0.1
+	**/
+	@Test
+	void contextLoads() {
+	
+	}
+}
