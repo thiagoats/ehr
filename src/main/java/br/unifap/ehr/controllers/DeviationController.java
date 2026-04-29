7544249c373efb7a31599a2417f49beb73b00312
Thiago Pinheiro do Nascimento <thiagopn.dev@gmail.com>
2026-04-29 15:49:05 -0300

    Deviation controller class

---
+++ src/main/java/br/unifap/ehr/controllers/DeviationController.java
@@ -0,0 +1,43 @@
+package br.unifap.ehr.controllers;
+
+import org.springframework.boot.webmvc.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.http.HttpServletRequest;
+
+/**
+ * Deviation controller class
+ * @author Thiago Pinheiro do Nascimento
+ * @since 0.1
+**/
+@Controller
+public class DeviationController implements ErrorController {
+
+	/**
+	 * Redirecting to error pages views
+	 * @author Thiago Pinheiro do Nascimento
+	 * @since 0.1
+	**/
+	@RequestMapping("/error")
+	public String handleError(HttpServletRequest request) {
+		Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+		if(status != null) {
+			Integer statusCode = Integer.valueOf(status.toString());
+			if(statusCode == HttpStatus.BAD_REQUEST.value())
+				return "errors/400";
+			if(statusCode == HttpStatus.FORBIDDEN.value())
+				return "errors/403";
+			if(statusCode == HttpStatus.NOT_FOUND.value())
+				return "errors/404";
+			if(statusCode == HttpStatus.CONFLICT.value())
+				return "errors/409";
+			if(statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value())
+				return "errors/500";
+			return "errors/500"; 
+		}
+		return "errors/500";
+	}
+}
