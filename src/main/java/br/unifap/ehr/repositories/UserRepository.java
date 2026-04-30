5b89abf224a46a6a5302d6726247c7be77eabf4b
Thiago Pinheiro do Nascimento <thiagopn.dev@gmail.com>
2026-04-30 18:00:08 -0300

    Find user by username

--- src/main/java/br/unifap/ehr/repositories/UserRepository.java
+++ src/main/java/br/unifap/ehr/repositories/UserRepository.java
@@ -1,5 +1,7 @@
 package br.unifap.ehr.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -13,4 +15,10 @@ import br.unifap.ehr.models.User;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>{
 
+	/**
+	 * Find user by username
+	 * @author Thiago Pinheiro do Nascimento
+	 * @since 0.1
+	**/
+	public Optional<User> findByUsername(String username);
 }
