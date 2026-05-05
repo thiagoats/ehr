29877a1e786b4eb5a12dfbe303d6664d3c9d53c9
Thiago Pinheiro do Nascimento <thiagopn.dev@gmail.com>
2026-05-05 14:31:44 -0300

    Adaptation of user details service implementation to account class

--- src/main/java/br/unifap/ehr/services/EhrUserDetailsService.java
+++ src/main/java/br/unifap/ehr/services/EhrUserDetailsService.java
@@ -10,31 +10,19 @@ import org.springframework.stereotype.Service;
 
 import br.unifap.ehr.models.User;
 import br.unifap.ehr.repositories.UserRepository;
+import br.unifap.ehr.security.Account;
 
-/**
- * Bridges the database user entities with Spring Security authentication.
- * @author Thiago Pinheiro do Nascimento
- * @since 0.1
-**/
 @Service
 public class EhrUserDetailsService implements UserDetailsService {
 
-	/**
-	 * Dependency injection with the user repository
-	**/
 	@Autowired
 	private UserRepository userRepository;
 	
-	/**
-	 * Loads user data from the database and maps it to Spring Security's UserDetails.
-	 * @author Thiago Pinheiro do Nascimento
-	 * @since 0.1
-	**/
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		Optional<User> user = userRepository.findByUsername(username);
 		if(user.isEmpty())
 			throw new UsernameNotFoundException("Usuário não encontrado");
-		return new org.springframework.security.core.userdetails.User(user.get().getUsername(), user.get().getPassword(), user.get().getAuthorities());
+		return new Account(user.get());
 	}
 }
