bd445fd5fc953d74a6bb9db78154b08ab5179fb4
Thiago Pinheiro do Nascimento <thiagopn.dev@gmail.com>
2026-05-06 14:57:33 -0300

    Audit properties in the user model class

--- src/main/java/br/unifap/ehr/models/User.java
+++ src/main/java/br/unifap/ehr/models/User.java
@@ -1,15 +1,22 @@
 package br.unifap.ehr.models;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.List;
 
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 import org.hibernate.validator.constraints.Length;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
@@ -23,65 +30,54 @@ import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
-/**
- * User model class
- * @author Thiago Pinheiro do Nascimento
- * @since 0.1
-**/
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 @Table(name = "users")
 public class User implements Serializable {
 	
-	/**
-	 * Serial version UID for backward compatibility during serialization
-	**/
 	private static final long serialVersionUID = 1L;
 	
-	/**
-	 * User id
-	**/
 	@EqualsAndHashCode.Include
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-	/**
-	 * User email
-	**/
 	@NotBlank(message = "E-mail é obrigatório")
 	@Length(min = 10, max = 60, message = "E-mail deve conter entre 10 e 60 caracteres")
 	@Email(message = "E-mail inválido")
 	@Column(length = 60, nullable = false, unique = true)
 	private String username;
 	
-	/**
-	 * User password
-	**/
 	@NotBlank(message = "Senha é obrigatória")
 	@Column(columnDefinition = "char(60)", nullable = false)
 	private String password;
 	
-	/**
-	 * User authority
-	**/
 	@Enumerated(EnumType.STRING)
 	@NotNull(message = "Perfil é obrigatório")
 	@Column(columnDefinition = "char(3)", nullable = false)
 	private Role role;
 	
-	/**
-	 * Indicates whether the user is enabled.
-	**/
 	@Column(columnDefinition = "tinyint(1) default 1", nullable = false)
 	private Boolean enabled;
 	
-	/**
-	 * Return the user authorities
-	 * @author Thiago Pinheiro do Nascimento
-	 * @since 0.1
-	**/
+	@CreationTimestamp
+	@Column(columnDefinition = "datetime", nullable = false, updatable = false)
+	private LocalDateTime createdAt;
+	
+	@CreatedBy
+	@Column(length = 60, nullable = false, updatable = false)
+	private String createdBy;
+	
+	@UpdateTimestamp
+	@Column(columnDefinition = "datetime", nullable = false)
+	private LocalDateTime updatedAt;
+	
+	@LastModifiedBy
+	@Column(length = 60, nullable = false)
+	private String updatedBy;
+	
 	public Collection<? extends GrantedAuthority> getAuthorities() {
 		return List.of(new SimpleGrantedAuthority(role.getAuthority()));
 	}
