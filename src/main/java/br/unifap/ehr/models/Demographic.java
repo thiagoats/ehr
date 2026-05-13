90a9b1aa139254e845da1541620393f2764a2fd7
Thiago Pinheiro do Nascimento <thiagopn.dev@gmail.com>
2026-05-13 15:58:15 -0300

    Demographic model class

---
+++ src/main/java/br/unifap/ehr/models/Demographic.java
@@ -0,0 +1,60 @@
+package br.unifap.ehr.models;
+
+import java.util.Date;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.MapsId;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "patient_demographics")
+public class Demographic {
+
+	@Id
+	@Column(name = "patients_id")
+	private Long id;
+	
+	@DateTimeFormat(pattern = "dd/MM/yyyy")
+	@NotNull(message = "Nascimento é obrigatório")
+	@Column(columnDefinition = "date", nullable = false)
+	private Date birth;
+	
+	@Enumerated(EnumType.STRING)
+	private Gender gender;
+	
+	@Enumerated(EnumType.STRING)
+	private Modality modality;
+	
+	@Enumerated(EnumType.STRING)
+	private Orientation orientation;
+	
+	@Enumerated(EnumType.STRING)
+	private Religion religion;
+	
+	@Enumerated(EnumType.STRING)
+	private Ethnicity ethnicity;
+	
+	@Enumerated(EnumType.STRING)
+	private Marital marital;
+	
+	@Enumerated(EnumType.STRING)
+	private Disability disability;
+	
+	@OneToOne
+	@MapsId
+	@JoinColumn(name = "patients_id")
+	private Patient patient;
+}
