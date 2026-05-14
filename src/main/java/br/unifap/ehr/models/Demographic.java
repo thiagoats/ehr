b1fd17a2aad3ec9c711c8082fa18a4364db49520
Thiago Pinheiro do Nascimento <thiagopn.dev@gmail.com>
2026-05-14 16:23:23 -0300

    Association between address and demographic model classes

--- src/main/java/br/unifap/ehr/models/Demographic.java
+++ src/main/java/br/unifap/ehr/models/Demographic.java
@@ -4,6 +4,7 @@ import java.util.Date;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -12,7 +13,9 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.MapsId;
 import jakarta.persistence.OneToOne;
+import jakarta.persistence.PrimaryKeyJoinColumn;
 import jakarta.persistence.Table;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
@@ -57,4 +60,9 @@ public class Demographic {
 	@MapsId
 	@JoinColumn(name = "patients_id")
 	private Patient patient;
+	
+	@Valid
+	@OneToOne(mappedBy = "demographic", cascade = CascadeType.ALL)
+	@PrimaryKeyJoinColumn
+	private Address address;
 }
